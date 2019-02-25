e3570c91fbbab201b34b12403bf79b01acde3014
2dearsue <2dearsue@gmail.com>
2019-02-25 10:52:56 +0100

    intro started

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/main.scss';
-import Navbar from './Navbar';
+import Intro from './Intro';
 
 class App extends Component {
 
   render() {
     return (
       <section>
-        <Navbar />
+        <Intro />
       </section>
     );
   }
