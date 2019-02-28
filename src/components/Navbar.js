6f0cad5c486510c4525328238e93b29fe8039d1b
2dearsue <2dearsue@gmail.com>
2019-03-01 00:26:46 +0100

    nav routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,44 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-     navbarclass: false
+     showNavbar: false
         };
       this.addActiveClass=this.addActiveClass.bind(this);
       this.showNavbar=this.showNavbar.bind(this);
     }
 
   addActiveClass() {
-      const toggleHamburger = () => {
-        document.getElementById("hamburger").classList.add("is-active");
-      };
-      toggleHamburger();
-    }
+      this.refs.btn.classList.toggle('is-active');
+
+      if (this.state.showNavbar === true) {
+        this.setState({showNavbar: false})
+      } else {
+        this.setState({showNavbar: true});
+      }
+        // document.getElementById("hamburger").classList.toggle("is-active");
+  }
 
   showNavbar() {
-    this.setState({navbarclass: true})
+
   }
 
   render() {
     return (
       <section>
-      <nav className="navbar navbar-expand-lg navbar-light">
+      <nav className="navbar navbar-expand-lg navbar-light mt-2 mx-auto">
 
         <div className="col-xs-2">
           <button
+              ref="btn"
               className="btn hamburger hamburger--squeeze"
               id="hamburger"
               type="button"
-              onClick={this.toggleHamburger}
+              onClick={this.addActiveClass}
               >
             <span className="hamburger-box">
               <span className="hamburger-inner"></span>
@@ -41,14 +47,16 @@ class Navbar extends Component {
         </div>
 
           {(this.state.showNavbar &&
-            <div className="navbar-collapse hidden"
+            <div className="navbar-collapse "
                 id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active border-2">
-                <span className="nav-link text-white text-center border-white border" id="2">About Me</span>
+                <NavLink to="/aboutMe" className="nav-link"><span className="nav-link text-white text-center border-white border" id="2">About Me</span>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <span className="nav-link text-white text-center" id="3">Skills</span>
+                <NavLink to="/skills" className="nav-link"><span className="nav-link text-white text-center">Skills</span>
+                </NavLink>
               </li>
               <li className="nav-item dropdown">
                 <span className="nav-link text-white text-center" id="4">Download CV</span>
